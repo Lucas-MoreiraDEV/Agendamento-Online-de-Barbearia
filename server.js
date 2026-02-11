e0fc84b7851ea6429c4b5e79921742388dd2c41e
Lucas KDev <lucas.moreira.kdev@gmail.com>
2026-02-11 17:27:57 -0300

    estruturaBasica

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
